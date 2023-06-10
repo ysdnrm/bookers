d6d65af7f77e51840059d077d251697c2a029b51
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-10 14:31:58 +0000

    [Add] 投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,21 @@
 class BooksController < ApplicationController
-  def index
+
+  def new
     @book = Book.new
+  end
+
+  def index
     @books = Book.all
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end  
   end
 
   def show
@@ -17,12 +25,18 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
-  end  
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book.id)
+  end
 
   private
   def book_params
