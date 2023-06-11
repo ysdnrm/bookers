1bfa3eee306eea2b5138a33eae4f3dba55cbaaa8
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-11 06:36:21 +0000

    [Add] バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,21 @@
 class BooksController < ApplicationController
 
-  def new
-    @book = Book.new
-  end
+
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
-    end  
+    end
   end
 
   def show
@@ -27,19 +27,35 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+
+     @book = Book.find(params[:id])
+     
+　　# 更新内容（book_params）が入っているのが@book
+　　# バリデーションの実装は流れが大事
+     if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
+     else
+      # createとは違い、変数の再定義はいらない
+      render :edit
+     end
+
+
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path(book.id)
   end
 
   private
   def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def update_params
     params.require(:book).permit(:title, :body)
   end
 end
