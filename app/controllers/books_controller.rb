e76ef3b3bd80a7a2e208240c4e311af9dfbca922
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-12 06:34:43 +0000

    [Finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    
   end
 
   def create
@@ -47,7 +48,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully destroyed."
-    redirect_to books_path(book.id)
+    redirect_to books_path
   end
 
   private
